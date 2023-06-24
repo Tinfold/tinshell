61eeedfb119d824698f4bcf90e36b2bc6e38bd05
Tinfold <inumeration@gmail.com>
2023-06-23 22:06:54 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,18 +1,3 @@
-# Bubble Shell
+# tinshell
 
-Bubble shell is a learning project in the spirit of the [build-your-own-x](https://github.com/danistefanovic/build-your-own-x) list. In cases where there was a trade-off between simplicity and robustness I most often chose simplicity.
-
-Check out the associated blog post - [Build Your Own Shell using Rust](https://www.joshmcguigan.com/blog/build-your-own-shell-rust/). 
-
-## License
-
-Licensed under either of
-
- * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
- * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
-
-at your option.
-
-### Contribution
-
-Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
+This is my own custom shell, built in Rust.
--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,121 @@
-use std::env;
+extern crate colored;
+extern crate crossterm;
+extern crate ctrlc;
+use colored::control;
+use colored::Colorize;
+use commands::commands_map;
+use crossterm::terminal;
+use crossterm::terminal::Clear;
+use crossterm::terminal::SetTitle;
+use crossterm::{execute, Result};
 use std::io::{stdin, stdout, Write};
-use std::path::Path;
 use std::process::{Child, Command, Stdio};
+use std::sync::mpsc::channel;
 
-fn main() {
+use shared::get_dir;
+use shared::set_dir;
+
+mod commands;
+mod shared;
+
+fn main() -> Result<()> {
+    let _ = control::set_virtual_terminal(true);
+    execute!(stdout(), SetTitle("tinshell"))?;
+    execute!(stdout(), Clear(terminal::ClearType::All))?;
+
+    let map = commands_map();
+    let files_per_row = 7;
+    // Only needed for windows
+    let (tx, rx) = channel();
+
+    // This effectively prevents closing the application with CTRL-C
+    ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
+        .expect("Error setting Ctrl-C handler");
+
+    // Set dir to whatever environment's dir is
+    set_dir(&get_dir());
     loop {
         // use the `>` character as the prompt
         // need to explicitly flush this to ensure it prints before read_line
+        println!(
+            "\n{}",
+            String::from(
+                "[".to_owned()
+                    + ("tinshell".truecolor(48, 153, 117)).to_string().as_str()
+                    + "]"
+                    + " -> "
+                    + get_dir().as_str()
+                    + "> "
+            )
+        );
         print!("> ");
         stdout().flush().unwrap();
 
         let mut input = String::new();
-        stdin().read_line(&mut input).unwrap();
+        match stdin().read_line(&mut input) {
+            Ok(v) => {
+                // read_line leaves a trailing newline, which trim removes
+                // this needs to be peekable so we can determine when we are on the last command
+                let mut commands = input.trim().split(" | ").peekable();
+                let mut previous_command = None;
 
-        // read_line leaves a trailing newline, which trim removes
-        // this needs to be peekable so we can determine when we are on the last command
-        let mut commands = input.trim().split(" | ").peekable();
-        let mut previous_command = None;
+                while let Some(command) = commands.next() {
+                    // everything after the first whitespace character is interpreted as args to the command
+                    let mut parts = command.trim().split_whitespace();
+                    let command = parts.next().unwrap_or("");
+                    let args = parts;
+                    // Check for command name in hashmap.
 
-        while let Some(command) = commands.next() {
-            // everything after the first whitespace character is interpreted as args to the command
-            let mut parts = command.trim().split_whitespace();
-            let command = parts.next().unwrap();
-            let args = parts;
-
-            match command {
-                "cd" => {
-                    // default to '/' as new directory if one was not provided
-                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                    let root = Path::new(new_dir);
-                    if let Err(e) = env::set_current_dir(&root) {
-                        eprintln!("{}", e);
+                    // if it is a special/reserved command like "exit", match it manually here.
+                    match command {
+                        "exit" => return Ok(()),
+                        command => {}
                     }
 
-                    previous_command = None;
-                }
-                "exit" => return,
-                command => {
-                    let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| {
-                        Stdio::from(output.stdout.unwrap())
-                    });
-
-                    let stdout = if commands.peek().is_some() {
-                        // there is another command piped behind this one
-                        // prepare to send output to the next command
-                        Stdio::piped()
+                    if (map.contains_key(command)) {
+                        // If so, run that command with args.
+                        map.get(command).unwrap().command(&args);
                     } else {
-                        // there are no more commands piped behind this one
-                        // send output to shell stdout
-                        Stdio::inherit()
-                    };
+                        let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| {
+                            Stdio::from(output.stdout.unwrap())
+                        });
 
-                    let output = Command::new(command)
-                        .args(args)
-                        .stdin(stdin)
-                        .stdout(stdout)
-                        .spawn();
+                        let stdout = if commands.peek().is_some() {
+                            // there is another command piped behind this one
+                            // prepare to send output to the next command
+                            Stdio::piped()
+                        } else {
+                            // there are no more commands piped behind this one
+                            // send output to shell stdout
+                            Stdio::inherit()
+                        };
 
-                    match output {
-                        Ok(output) => {
-                            previous_command = Some(output);
-                        }
-                        Err(e) => {
-                            previous_command = None;
-                            eprintln!("{}", e);
-                        }
-                    };
+                        let output = Command::new(command)
+                            .args(args)
+                            .stdin(stdin)
+                            .stdout(stdout)
+                            .spawn();
+
+                        match output {
+                            Ok(output) => {
+                                previous_command = Some(output);
+                            }
+                            Err(e) => {
+                                previous_command = None;
+                                eprintln!("{}", e);
+                            }
+                        };
+                    }
+                }
+
+                if let Some(mut final_command) = previous_command {
+                    // block until the final command has finished
+                    final_command.wait().unwrap();
                 }
             }
-        }
-
-        if let Some(mut final_command) = previous_command {
-            // block until the final command has finished
-            final_command.wait().unwrap();
+            Err(e) => {
+                println!("{}", e)
+            }
         }
     }
 }
