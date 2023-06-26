c502745d5fd0a08ff9ce157d2e97bc00eda92c39
Tinfold <inumeration@gmail.com>
2023-06-25 22:06:03 -0400

    Some refactoring

--- tinshell/src/commands/test.rs
+++
@@ -1,9 +0,0 @@
-use shlex::Shlex;
-use std::io::{self, stdout, Write};
-pub fn test(args: Shlex) {
-    let mut stdout = io::stdout().lock();
-    match stdout.write_all(b"hello world") {
-        Ok(_v) => {}
-        Err(_e) => {}
-    };
-}
