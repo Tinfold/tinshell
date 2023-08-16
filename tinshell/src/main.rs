07c5dde474f65fb704fd1ecb98580a505b9f2872
Tinfold <inumeration@gmail.com>
2023-08-16 11:11:11 -0400

    Refactoring set up some syntax highlighting and tab completion functionality

--- tinshell/src/main.rs
+++ tinshell/src/main.rs
@@ -12,8 +12,17 @@ use commands::commands_map;
 use crossterm::terminal::SetTitle;
 use crossterm::{execute, Result};
 use nu_ansi_term::{Color, Style};
+use reedline::default_emacs_keybindings;
+use reedline::ColumnarMenu;
+use reedline::DefaultCompleter;
+use reedline::Emacs;
+use reedline::KeyCode;
+use reedline::KeyModifiers;
+use reedline::ReedlineEvent;
+use reedline::ReedlineMenu;
 use reedline::{
-    DefaultHinter, DefaultPrompt, DefaultPromptSegment, DefaultValidator, Reedline, Signal,
+    DefaultHinter, DefaultPrompt, DefaultPromptSegment, DefaultValidator, ExampleHighlighter,
+    Reedline, Signal,
 };
 use shlex::Shlex;
 
@@ -26,6 +35,39 @@ use std::sync::mpsc::channel;
 mod commands;
 
 fn main() -> Result<()> {
+    let tabs = vec![
+        "git commit".into(),
+        "git push".into(),
+        "git checkout".into(),
+    ];
+    let completer = Box::new(DefaultCompleter::new_with_wordlen(tabs.clone(), 2));
+    // Use the interactive menu to select options from the completer
+    let completion_menu = Box::new(ColumnarMenu::default().with_name("completion_menu"));
+    // Set up the required keybindings
+    let mut keybindings = default_emacs_keybindings();
+    keybindings.add_binding(
+        KeyModifiers::NONE,
+        KeyCode::Tab,
+        ReedlineEvent::UntilFound(vec![
+            ReedlineEvent::Menu("completion_menu".to_string()),
+            ReedlineEvent::MenuNext,
+        ]),
+    );
+
+    let edit_mode = Box::new(Emacs::new(keybindings));
+
+    // need to make automatic resolver for this
+    let syntax_highlighting = vec![
+        "cd".into(),
+        "del".into(),
+        "ls".into(),
+        "md".into(),
+        "mf".into(),
+        "mv".into(),
+        "exit".into(),
+        "git".into(),
+    ];
+
     //let completer = Box::new(DefaultCompleter::new(external_commands));
     let validator = Box::new(DefaultValidator);
 
@@ -33,12 +75,17 @@ fn main() -> Result<()> {
         .with_validator(validator)
         .with_hinter(Box::new(
             DefaultHinter::default().with_style(Style::new().italic().fg(Color::LightGray)),
-        ));
+        ))
+        .with_highlighter(Box::new(ExampleHighlighter::new(syntax_highlighting)))
+        .with_completer(completer)
+        .with_menu(ReedlineMenu::EngineCompleter(completion_menu))
+        .with_edit_mode(edit_mode);
 
     // Eventually need to replace the crappy unicode > character for the prompt. For now, this will do.
 
-    let left_prompt = DefaultPromptSegment::Empty;
+    let left_prompt = DefaultPromptSegment::Basic("".to_string());
     let right_prompt = DefaultPromptSegment::Empty;
+
     let prompt = DefaultPrompt::new(left_prompt, right_prompt);
 
     execute!(stdout(), SetTitle("tinshell"))?;
